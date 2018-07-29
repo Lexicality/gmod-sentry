d404d3a7bf4218698b5e699c106ce1feea93e031
Lex Robinson <lex@lex.me.uk>
2018-07-29 18:50:15 +0100

    Sort out the error file a bunch more
    Now that I've got an addon for more complex errors, make this as easy for
    testing as useful.
    
    Since Error and ErrorNoHalt don't produce stacktraces,
    don't do anything with them.

--- lua/autorun/errors.lua
+++ lua/autorun/errors.lua
@@ -1,30 +1,25 @@
-local function throws(func, arg1, arg2)
+local function throws(arg1, arg2)
 	local foo = 8;
 	local bar = arg1 .. arg2;
-	func("oops");
+	error("oops");
 end
 
-local function level1(func, arg1)
+local function level1(arg1)
 	local two = "beans";
-	throws(func, arg1, two);
+	throws(arg1, two);
 end
 
 local function level2(func)
-	level1(func, "beans");
+	level1("beans");
 end
 
-local function level3(func) level2(func) end
-local function level4(func) level3(func) end
-local function level5(func) level4(func) end
+local function level3() level2() end
+local function level4() level3() end
+local function level5() level4() end
 
 
 if SERVER then
-	concommand.Add("oops", function() level5(error) end);
-	concommand.Add("oops2", function() level5(Error) end);
-	concommand.Add("oops3", function() level5(ErrorNoHalt) end);
-	concommand.Add("oops4", function() error("oops") end);
+	concommand.Add("oops", function() level5() end);
 else
-	concommand.Add("cl_oops", function() level5(error) end);
-	concommand.Add("cl_oops2", function() level5(Error) end);
-	concommand.Add("cl_oops3", function() level5(ErrorNoHalt) end);
+	concommand.Add("cl_oops", function() level5() end);
 end
